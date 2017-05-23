5d0c5fd7bbb342e87210c84220ac3b94dbb016c6
Kees Postma <kk.postma@gmail.com>
2017-05-23 14:58:21 +0100

    Completes basic CRUD functionality of todo list manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do
-  # READ
-  # All
-  get '/tasks', to: 'tasks#index'
-  # One
-  get '/tasks/:id', to: 'tasks#show'
+  get '/', to: 'tasks#index'
 
-  # CREATE
-  # form
-  get '/tasks/:id/new', to: 'tasks#new'
-  # create
-  post '/tasks', to: 'tasks#create'
-
-  # UPDATE
-  # form
-  get '/tasks/:id/edit', to: 'tasks#edit'
-  # update
-  patch '/tasks/:id', to: 'tasks#update'
-
-  # DELETE
-  delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
