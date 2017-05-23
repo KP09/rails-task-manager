8f2d6add6e4bc4716e04a69cac03f829e02c66c9
Kees Postma <kk.postma@gmail.com>
2017-05-23 12:10:47 +0100

    Creates Task model with name, description, and due date. Draws 7 standard routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ
+  # All
+  get '/tasks', to: 'tasks#index'
+  # One
+  get '/tasks/:id', to: 'tasks#show'
+
+  # CREATE
+  # form
+  get '/tasks/:id/new', to: 'tasks#new'
+  # create
+  post '/tasks', to: 'tasks#create'
+
+  # UPDATE
+  # form
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  # update
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
